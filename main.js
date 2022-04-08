0d53e1b0a4f4d9cb2f6ec655ae78d421ccf910df
unknown <martinsozols8@gmail.com>
2022-04-08 12:08:12 +0300

    nice

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
-x=document.getElementById("vardi").innerHTML
-m = x.split(' ')
-m[9] = "<span style=\"color:red\">" + m[10] + "</span>"
-y = m.join(" ")
-document.getElementById("vardi").innerHTML = y
+
